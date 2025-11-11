e38b85286b86fb6ef499832c34a2f652df896ea8
Всеволод Донченко <vsevolond@MacBook-Air-Vsevolod.local>
2025-11-11 18:49:52 +0300

    initial commit

---
+++ pageflow-studio/pageflow_studioApp.swift
@@ -0,0 +1,17 @@
+//
+//  pageflow_studioApp.swift
+//  pageflow-studio
+//
+//  Created by Всеволод Донченко on 11.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct pageflow_studioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
